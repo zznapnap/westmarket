0ed1246a4fdb12883b43bfc367d3dca590ae17ff
zznapnap <zznapnap@gmail.com>
2016-04-14 00:38:39 +0800

    input and find immediate

--- http/searchName.js
+++ http/searchName.js
@@ -1,12 +1,12 @@
 function searchStudentName(){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if(xhttp.readyState == 4 && xhttp.status == 200){
-			document.getElementById("searchResult").innerHTML = xhttp.responseText;
-		}
-	};
-	var url = "do.njs?userID=" + document.getElementById("userID").value;	
-	xhttp.open("GET", url, true);
-	xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(xhttp.readyState == 4 && xhttp.status == 200){
+      document.getElementById("searchResult").innerHTML = xhttp.responseText;
+    }
+  };
+  var url = "do.njs?userID=" + document.getElementById("userID").value;
+  xhttp.open("GET", url, true);
+  xhttp.send();
 
 }
